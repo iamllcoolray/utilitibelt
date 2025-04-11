a5a0771e23a0656bf4adbf274a696f5de4bcaa2a
iamllcoolray <llcoolray24@gmail.com>
2025-04-11 01:39:41 -0400

    upon creating a new project, the newly generated project will output the litiengine template

--- src/main/java/com/nobunagastudios/utilitibelt/UtilitiBeltProjectWizard.java
+++ src/main/java/com/nobunagastudios/utilitibelt/UtilitiBeltProjectWizard.java
@@ -4,13 +4,17 @@ import com.intellij.icons.AllIcons;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.ide.wizard.NewProjectWizardStep;
 import com.intellij.ide.wizard.language.LanguageGeneratorNewProjectWizard;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.observable.properties.PropertyGraph;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.UserDataHolder;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.dsl.builder.Panel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class UtilitiBeltProjectWizard implements LanguageGeneratorNewProjectWizard {
     @Override
@@ -48,7 +52,64 @@ public class UtilitiBeltProjectWizard implements LanguageGeneratorNewProjectWiza
 
             @Override
             public void setupProject(@NotNull Project project) {
-                NewProjectWizardStep.super.setupProject(project);
+                ApplicationManager.getApplication().runWriteAction(() -> {
+                    try {
+                        String basePath = getContext().getProjectFileDirectory();
+                        VirtualFile baseDir = VfsUtil.createDirectoryIfMissing(basePath);
+                        if (baseDir == null) throw new IOException("Failed to create base directory");
+
+                        VirtualFile src = VfsUtil.createDirectoryIfMissing(baseDir, "src");
+                        VirtualFile main = VfsUtil.createDirectoryIfMissing(src, "main");
+                        VirtualFile java = VfsUtil.createDirectoryIfMissing(main, "java");
+                        VirtualFile resources = VfsUtil.createDirectoryIfMissing(main, "resources");
+                        VfsUtil.createDirectoryIfMissing(resources, "audio");
+                        VfsUtil.createDirectoryIfMissing(resources, "sprites");
+                        VfsUtil.createDirectoryIfMissing(resources, "maps");
+                        VfsUtil.createDirectoryIfMissing(resources, "localization");
+                        VfsUtil.createDirectoryIfMissing(resources, "misc");
+
+                        VirtualFile mainFile = java.createChildData(this, "Main.java");
+                        mainFile.setBinaryContent((
+                                "import de.gurkenlabs.litiengine.*;\n\n" +
+                                        "public class Main {\n" +
+                                        "    public static void main(String[] args) {\n" +
+                                        "        Game.init(args);\n" +
+                                        "        Game.start();\n" +
+                                        "    }\n" +
+                                        "}"
+                        ).getBytes());
+
+                        VirtualFile buildGradle = baseDir.createChildData(this, "build.gradle");
+                        buildGradle.setBinaryContent((
+                                "plugins {\n" +
+                                        "    id 'java'\n" +
+                                        "    id 'application'\n" +
+                                        "}\n\n" +
+                                        "group 'com.example'\n" +
+                                        "version '1.0'\n\n" +
+                                        "repositories {\n" +
+                                        "    mavenCentral()\n" +
+                                        "    maven { url 'https://maven.pkg.jetbrains.space/litiengine/p/maven/releases' }\n" +
+                                        "}\n\n" +
+                                        "dependencies {\n" +
+                                        "    implementation 'de.gurkenlabs:litiengine:0.8.0'\n" +
+                                        "}\n\n" +
+                                        "application {\n" +
+                                        "    mainClass = 'Main'\n" +
+                                        "}"
+                        ).getBytes());
+
+                        String projectName = getContext().getProjectName();
+
+                        VirtualFile settingsGradle = baseDir.createChildData(this, "settings.gradle");
+                        settingsGradle.setBinaryContent(
+                                ("rootProject.name = '" + projectName + "'").getBytes()
+                        );
+
+                    } catch (IOException e) {
+                        throw new RuntimeException("Failed to generate LITIENGINE project", e);
+                    }
+                });
             }
 
             @Override
