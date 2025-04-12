20dcd0a7c69305c013874160e54f8d923357d4e7
iamllcoolray <llcoolray24@gmail.com>
2025-04-12 01:00:38 -0400

    moved the UtilitiBeltProjectWizardAction into an actions package

--- src/main/java/com/nobunagastudios/utilitibelt/actions/UtilitiBeltProjectWizardAction.java
+++ src/main/java/com/nobunagastudios/utilitibelt/actions/UtilitiBeltProjectWizardAction.java
@@ -1,13 +1,13 @@
-package com.nobunagastudios.utilitibelt;
+package com.nobunagastudios.utilitibelt.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
-public class UtilitiBeltProjectAction extends AnAction {
+public class UtilitiBeltProjectWizardAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        Messages.showInfoMessage("New LITIENGINE Project Wizard triggered!", "utiLITI Belt Project Wizard");
+        Messages.showInfoMessage(e.getProject(), "New LITIENGINE Project Wizard triggered!", "utiLITI Belt");
     }
 }
